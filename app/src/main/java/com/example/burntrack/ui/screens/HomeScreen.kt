c3d3c10bc0386f4c9600251b933a01a49405b126
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-03-06 17:13:56 +0200

    Add image to home screen

--- app/src/main/java/com/example/burntrack/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/burntrack/ui/screens/HomeScreen.kt
@@ -1,14 +1,17 @@
 package com.example.burntrack.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.burntrack.R
 import com.example.burntrack.ui.components.AppTitle
 import com.example.burntrack.ui.components.MainTopBar
 
@@ -23,7 +26,13 @@ fun HomeScreen(navController: NavController) {
                 .fillMaxWidth()
                 .padding(innerPadding)
         ) {
-
+            Image(
+                painter = painterResource(id = R.drawable.body),
+                contentDescription = "BurnTrack home",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
         }
     }
 
