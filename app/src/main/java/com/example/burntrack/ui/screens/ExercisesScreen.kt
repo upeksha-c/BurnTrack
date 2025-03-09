e1931c2a65c5ad797afbd4dd23b9da5d286dadb3
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-03-09 11:46:03 +0200

    Show exercise gif in screen

--- app/src/main/java/com/example/burntrack/ui/screens/ExercisesScreen.kt
+++ app/src/main/java/com/example/burntrack/ui/screens/ExercisesScreen.kt
@@ -1,5 +1,7 @@
 package com.example.burntrack.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,9 +15,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.burntrack.model.Exercise
 import com.example.burntrack.ui.components.ScreenTopAppBar
 import com.example.burntrack.viewmodel.ExercisesViewModel
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun ExercisesScreen(navController: NavController, modifier: Modifier = Modifier, bodyPart: String, exercisesViewModel : ExercisesViewModel = viewModel()){
@@ -40,10 +44,24 @@ fun ExerciseCardList(modifier: Modifier = Modifier, exercises: List<Exercise>) {
     ){
         items(exercises) { exercise ->
             Text(
-                text = exercise.name,
+                text = exercise.name.toTitleFormat(),
                 modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+
+            // Display the GIF image for the exercise
+            GlideImage(
+                imageModel = { exercise.gifUrl },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(350.dp)
             )
             HorizontalDivider(color = Color.LightGray, thickness = 2.dp)
         }
     }
+}
+
+fun String.toTitleFormat(): String {
+    val words = this.split(" ")
+    val camelCaseString = words.joinToString(" ") { it.replaceFirstChar { char -> char.uppercase() } }
+    return camelCaseString
 }
