5840bb7b3f12a740ea85cf0e436237a3f9d9405c
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-03-09 13:03:26 +0200

    Add exercise details into a card

--- app/src/main/java/com/example/burntrack/ui/screens/ExercisesScreen.kt
+++ app/src/main/java/com/example/burntrack/ui/screens/ExercisesScreen.kt
@@ -1,10 +1,13 @@
 package com.example.burntrack.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
 import com.example.burntrack.model.Exercise
 import com.example.burntrack.ui.components.ScreenTopAppBar
 import com.example.burntrack.viewmodel.ExercisesViewModel
@@ -45,29 +47,39 @@ fun ExerciseCardList(modifier: Modifier = Modifier, exercises: List<Exercise>) {
             .padding(16.dp)
     ){
         items(exercises) { exercise ->
-            Text(
-                text = exercise.name.toTitleFormat(),
-                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp),
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp
-            )
+            Card (
+                modifier = Modifier.fillMaxWidth().padding(top = 5.dp, bottom = 10.dp),
+                elevation  = CardDefaults.elevatedCardElevation(defaultElevation = 10.dp)
+            ){
+                Column (
+                    modifier = Modifier.padding(15.dp)
+                ) {
+                    Text(
+                        text = exercise.name.toTitleFormat(),
+                        modifier = Modifier.padding(top = 4.dp, bottom = 4.dp),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp
+                    )
 
-            // Display the GIF image for the exercise
-            GlideImage(
-                imageModel = { exercise.gifUrl },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(350.dp)
-            )
+                    // Display the GIF image for the exercise
+                    GlideImage(
+                        imageModel = { exercise.gifUrl },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(350.dp)
+                            .padding(top = 4.dp, bottom = 4.dp)
+                    )
 
-            //display instructions
-            exercise.instructions.forEachIndexed() { index,instruction ->
-                Text(
-                    text = "${index + 1}. $instruction",
-                    modifier = Modifier.padding(top = 9.dp)
-                )
+                    //display instructions
+                    exercise.instructions.forEachIndexed { index,instruction ->
+                        Text(
+                            text = "${index + 1}. $instruction",
+                            modifier = Modifier.padding(top = 9.dp)
+                        )
+                    }
+                }
             }
-            HorizontalDivider(color = Color.LightGray, thickness = 2.dp, modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
+
         }
     }
 }
